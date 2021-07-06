bd785c927c020c655f5e2b205441719667cfa8fc
Greg Nemeth <GNemeth@qa.com>
2021-07-06 17:00:39 +0000

    GN: partially working code

--- easy/near.py
+++ easy/near.py
@@ -1,2 +1,17 @@
-string_a = 
-string_b = 
+
+string_a = 'dragoon'
+string_b = 'dragon'
+
+
+def near(string_a, string_b):
+    counter = 0
+    for i in range(len(string_a) + 1):
+        new_string = string_a.replace(string_a[counter], '', 1)
+        
+        counter +=1
+        if new_string == string_b:
+            break
+    return new_string == string_b
+  
+
+print(near(string_a, string_b))
