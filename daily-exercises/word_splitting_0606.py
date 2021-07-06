7db3e031b891f15a81a3e38375be6341d8981435
Greg Nemeth <GNemeth@qa.com>
2021-07-06 08:13:12 +0000

    GN:convert to string

--- daily-exercises/word_splitting_0606.py
+++ daily-exercises/word_splitting_0606.py
@@ -3,6 +3,6 @@ sentence = input('Please enter a sentence:')
 split_sentence = sentence.split(' ')
 
 split_set = set(split_sentence)
-ordered = sorted(split_set, key=str.lower)
+ordered = ' '.join(sorted(split_set, key=str.lower))
 
 print(ordered)
