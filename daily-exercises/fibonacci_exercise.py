e1cc35925478ee2fb8babb341011b45cc71c519a
Greg Nemeth <GNemeth@qa.com>
2021-07-06 17:01:51 +0000

    just the description

---
+++ daily-exercises/fibonacci_exercise.py
@@ -0,0 +1,19 @@
+The Fibonacci sequence is computed based on the following formula:
+
+
+f(n) = 0         if n=0
+f(n) = 1         if n=1
+f(n) = f(n-1) + f(n-2)    if n>1
+
+
+Basically, it is 0, 1, 1, 2, 3, 5, 8, 11, ...
+ 
+
+Write a program to compute the value of f(n) for any integer n.
+
+ 
+
+Examples:
+f(5) -> 5
+f(7) -> 13
+f(9) -> 34
