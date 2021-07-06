272bf52dae459c1e8a5d01787426ff7ce2851fc0
Greg Nemeth <GNemeth@qa.com>
2021-07-06 17:55:20 +0000

    GN: done

--- daily-exercises/fibonacci_exercise.py
+++ daily-exercises/fibonacci_exercise.py
@@ -1,19 +1,18 @@
-The Fibonacci sequence is computed based on the following formula:
 
+def fibonacci(n):
+    n1 = 0
+    n2 = 1
+    if n == 0:
+        return n1
+    elif n == 1:
+        return n2
+    else:
+        for i in range(1, n):
+            next = n1 + n2
+            print(n1)
+            n1 = n2
+            n2 = next
+        return n1
 
-f(n) = 0         if n=0
-f(n) = 1         if n=1
-f(n) = f(n-1) + f(n-2)    if n>1
-
-
-Basically, it is 0, 1, 1, 2, 3, 5, 8, 11, ...
- 
-
-Write a program to compute the value of f(n) for any integer n.
-
- 
-
-Examples:
-f(5) -> 5
-f(7) -> 13
-f(9) -> 34
+n = int(input("enter n:"))
+print(fibonacci(n))
