52a49f8a96cb8e783aeab571f3e0618cc04b8915
Greg Nemeth <GNemeth@qa.com>
2021-07-06 06:27:08 +0000

    GN:found a solution

--- daily-exercises/word_splitting_0606.py
+++ daily-exercises/word_splitting_0606.py
@@ -1,7 +1,8 @@
 sentence = input('Please enter a sentence:')
 
 split_sentence = sentence.split(' ')
-sorted_split = sorted(split_sentence)
-split_sorted_set = set(sorted_split)
 
-print(split_sorted_set)
+split_set = set(split_sentence)
+ordered = sorted(split_set, key=str.lower)
+
+print(ordered)
