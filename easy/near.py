798c7ba278bcd4804c741add4f1c8afb4e0f0ccf
Greg Nemeth <GNemeth@qa.com>
2021-07-07 14:55:17 +0000

    GN:

--- easy/near.py
+++ easy/near.py
@@ -4,14 +4,14 @@ string_b = 'dragon'
 
 
 def near(string_a, string_b):
-    counter = 0
-    for i in range(len(string_a) + 1):
-        new_string = string_a.replace(string_a[counter], '', 1)
-        
-        counter +=1
-        if new_string == string_b:
-            break
-    return new_string == string_b
-  
+
+    if len(string_b) != len(string_a)-1:
+        return False
+    else:
+        for i in range(len(string_a)):
+            if string_a[:i] + string_a[i+1:] == string_b:
+                return True
+        else:
+            return False
 
 print(near(string_a, string_b))
