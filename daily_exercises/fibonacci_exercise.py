9eeee436f3bae9dca7acc16ef274631e9a6fce84
Greg Nemeth <GNemeth@qa.com>
2021-07-07 10:33:17 +0000

    GN:fixed after testing

---
+++ daily_exercises/fibonacci_exercise.py
@@ -0,0 +1,18 @@
+
+def fibonacci(n):
+    n1 = 0
+    n2 = 1
+    if n == 0:
+        return n1
+    elif n == 1:
+        return n2
+    else:
+        for i in range(1, n):
+            next = n1 + n2
+            print(n1)
+            n1 = n2
+            n2 = next
+        return n2
+
+# n = int(input("enter n:"))
+# print(fibonacci(n))
